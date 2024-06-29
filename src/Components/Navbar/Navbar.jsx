28a8bd982445612022be5a2720367719f55856db
unknown <sakibsafat47@gmail.com>
2024-06-29 01:48:36 -0700

    html structure of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import './Navbar.css'
+import logo from "../Assets/logo.png"
+import cartIcon from "../Assets/cart_icon.png"
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="logo" />
+                <p>SHOP-E</p>
+            </div>
+            <ul className="nav-menu">
+                <li>
+                    Shop
+                </li>
+                <li>
+                    Men
+                </li>
+                <li>
+                    Women
+                </li>
+                <li>
+                    Kids
+                </li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>
+                    Log in
+                </button>
+                <img src={cartIcon} alt="icon" />
+            </div>
         </div>
     );
 };
