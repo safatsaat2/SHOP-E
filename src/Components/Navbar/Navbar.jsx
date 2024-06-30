d4f85396bb7ee2b668f3ccb559dca1f350b892e8
unknown <sakibsafat47@gmail.com>
2024-06-30 06:22:40 -0700

    linking to pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import './Navbar.css'
 import logo from "../Assets/logo.png"
 import cartIcon from "../Assets/cart_icon.png"
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop")
@@ -13,28 +14,39 @@ const Navbar = () => {
             </div>
             <ul className="nav-menu flex items-center gap-12 text-[#626262] font-medium justify-center">
                 <li onClick={()=>{setMenu("shop")}} className='flex items-center justify-center gap-1 cursor-pointer flex-col'>
-                    Shop
-                    {menu === "shop" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-[#FF4141]' /> : <></>}
+                    <Link to="/">Shop
+                    {menu === "shop" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-[#FF4141]' /> : <></>}</Link>
                 </li>
                 <li onClick={()=>{setMenu("men")}} className='flex items-center justify-center gap-1 cursor-pointer flex-col'>
+                    <Link to="/mens">
                     Men
                     {menu === "men" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-[#FF4141]' /> : <></>}
+                    </Link>
                 </li>
                 <li onClick={()=>{setMenu("women")}} className='flex items-center justify-center gap-1 cursor-pointer flex-col'>
+                    <Link to="/womens">
                     Women
                     {menu === "women" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-[#FF4141]' /> : <></>}
+                    </Link>
                 </li>
                 <li onClick={()=>{setMenu("kids")}} className='flex items-center justify-center gap-1 cursor-pointer flex-col'>
+                    <Link to="/kids">
                     Kids
                     {menu === "kids" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-[#FF4141]' /> : <></>}
+                    </Link>
                 </li>
             </ul>
             <div className="nav-login-cart flex items-center justify-center gap-11">
+                <Link to="/login">
                 <button className='w-[157px] h-[58px] border border-[#7a7a7a] rounded-[75px] text-xl font-medium bg-white text-[#515151] active:bg-[#f3f3f3]'>
                     Log in
                 </button>
+                </Link>
                 
+                <Link to="/cart">
                 <img src={cartIcon} alt="icon" />
+                
+                </Link>
                 <div className='w-[22px] h-[22px] flex justify-center items-center -mt-[35px] -ml-[55px] rounded-[11px] text-sm bg-red-600 text-white'>
                     0
                 </div>
